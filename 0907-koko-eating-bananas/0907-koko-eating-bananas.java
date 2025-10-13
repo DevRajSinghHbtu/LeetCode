96a41c35fd21a90404c849bb47200ac2d33a8d27
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2025-10-14 00:51:46 +0530

    Time: 24 ms (33.41%), Space: 45.3 MB (34.86%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int n = piles.length;
+        int last = piles[n-1];
+       long s = 1;
+       long e = last;
+       long ans = -1;
+       while(s<=e)
+       {
+        long mid  = s + (e-s)/2;
+        if(minicount(piles, mid)<=h)
+        {
+            ans = mid;
+            e = mid-1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+       }
+        return (int)ans;
+    }
+    long minicount(int [] nums, long hour)
+    {
+        long takentime = 0;
+        int n = nums.length;
+        for(int i = 0;i<n;i++)
+        {
+            takentime = takentime + (long)Math.ceil((double)nums[i]/hour);
+        }
+        return takentime;
+    }
+}
