19268e8ce355e7b8f3d4251c88ab7270110fef94
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2026-01-18 16:52:20 +0530

    Time: 3 ms (53.31%), Space: 45.2 MB (49.45%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -3,14 +3,22 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         ans.add(new ArrayList<>());
-        for(int num : nums)
+         int start = 0;
+         int end = 0;
+        for(int i = 0; i<nums.length; i++)
         {
-            int n = ans.size();
-            for(int i = 0; i<n; i++)
+            start = 0;
+            if(i>0 && nums[i]==nums[i-1])
             {
-                 List<Integer> ds = new ArrayList<>(ans.get(i));
-                 ds.add(num);
-                 if(!ans.contains(ds))
+                start = end + 1;
+            }
+            end = ans.size() - 1;
+            int n = ans.size();
+            for(int j = start; j<n; j++)
+            {
+                 List<Integer> ds = new ArrayList<>(ans.get(j));
+                 ds.add(nums[i]);
+                 
                    ans.add(ds);
             }
             
