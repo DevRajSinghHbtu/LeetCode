c0e348843e87dc35de5909bd7ba366124735c854
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2026-01-29 15:36:57 +0530

    Time: 133 ms (11.19%), Space: 71.9 MB (6.54%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        int[] a = new int[n+1];
+        for(int i = 2; i<n; i++)
+        {
+            a[i] = 1;
+        }
+        int c = 0;
+        for(int i = 2; i<n; i++)
+        {
+            if(a[i]==1)
+            {
+                c++;
+                for(int j = 2*i; j<n; j += i)
+                {
+                    a[j] = 0;
+                }
+            }
+        }
+     return c;
+ }
+}
