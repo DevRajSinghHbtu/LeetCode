2bcb436cb3cc9242ea92b1bff6dc089bb179abb6
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2026-02-22 18:25:46 +0530

    Time: 30 ms (72.78%), Space: 54.4 MB (75.2%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -15,16 +15,7 @@ class StockSpanner {
       return span; 
     }
 
-      static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter f = new FileWriter("display_runtime.txt")) {
-                f.write("0");
-            } catch (Exception e) {
-
-            }
-        }));
-    }
+      
 }
 
 /**
