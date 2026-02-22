d8d6ec08e2abce0422bc790db598ecc8b3700e7c
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2026-02-22 22:12:00 +0530

    Time: 1267 ms (5.02%), Space: 157.8 MB (10.11%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(k == 1)
+        return nums;
+       int l = 0;
+       int r = k-1;
+       int max = Integer.MIN_VALUE;
+       int mi = -1;
+
+       for(int i = l; i<=r; i++)
+       {
+        if(max <= nums[i])
+        {
+            max = nums[i];
+            mi = i;
+        }
+       } 
+       int n = nums.length;
+      int[] a = new int[n-(k-1)];
+      int i = 0;
+    while(r<n)
+    {
+        if(max < nums[r] && mi >= l)
+        {
+            mi = r;
+            max = nums[r];
+        }
+        else if(mi <= l)
+        {
+            max = Integer.MIN_VALUE;
+            for(int j = l; j<=r; j++)
+            {
+                if(max <= nums[j])
+                 {
+                    max = nums[j];
+                    mi = j;
+                 }
+            }
+        }
+     a[i] = max;
+     r += 1;
+     l += 1;
+     i++;
+    }
+     
+        return a;
+    }
+}
